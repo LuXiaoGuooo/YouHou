9a3718dc31f4f9533557c5de0340e53d0b3444c0
陆小果 <luxiaoguo@foxmail.com>
2025-03-12 01:09:38 +0800

    你好世界

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Hello world
+// @namespace    https://Luxiaoguo.com
+// @version      1.0.0
+// @description  A script to log "Hello World" in console
+// @author       Luxiaoguo
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @license      AGPL-3.0
+// @grant         none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("你好世界 i like you")
+})();
